6e31ae971a3901d92c15bfe7d5521232a9958ee3
François <francois@beafrancois.fr>
2026-05-31 10:17:54 +0200

    removed unused robustness.

--- test/robustness/console terminal/generate_char.sh
+++
@@ -1,9 +0,0 @@
-chars='<=>| -,;:!/."()[]{}*\&#%+012345689abcdefghiklmnopqrstuvwxyzABCD'
-for j in {1..256} ;
-do
-    for i in {1..256} ; do
-        echo -n "${chars:RANDOM%${#chars}:1}"
-    done
-    echo
-    sleep 0.01
-done
