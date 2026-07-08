0b757b78cb8359b4fc7fb85656f44e72f66ec041
François <francois@beafrancois.fr>
2026-07-08 08:09:17 +0200

    now a clear error message is displayed when python venv is not installed on the PC

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ export PY_VENV_NAME=".venv"
 export PY_VENV_DIR="$SCRIPT_DIR/test/tmp/$PY_VENV_NAME"
 export REQ_PATH="$SCRIPT_DIR/src/requirements.txt"
 
-bash $SCRIPT_DIR/scripts/build_env.sh
+bash $SCRIPT_DIR/scripts/build_env.sh || exit 1
 source $SCRIPT_DIR/scripts/set_env.sh
 
 python3 "$SCRIPT_DIR/src/testium"  $@
